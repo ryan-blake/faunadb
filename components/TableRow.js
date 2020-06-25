14af9f68a0568d76b36341bba6cf98bb11864d9f
ryan-blake <ryan_blake@icloud.com>
2020-06-25 08:06:41 -0500

    f

--- components/TableRow.js
+++ components/TableRow.js
@@ -5,7 +5,7 @@ export default ({ creditCard, firstName, loading, lastName, telephone }) => (
     </p>{' '}
     <p className={`telephone ${loading ? 'loading' : ''}`}>{telephone}</p>
     <p className={`credit-card credit-card-number ${loading ? 'loading' : ''}`}>
-      {creditCard && <img width="100" height="64" src="https://www.iconninja.com/files/801/537/209/payment-visa-method-card-icon.png" />}
+      {creditCard && <img width="40" height="40" src="https://www.iconninja.com/files/801/537/209/payment-visa-method-card-icon.png" />}
       {creditCard}
     </p>
   </div>
